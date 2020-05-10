fde37354ca116dba31758536370e9bc78f2766b4
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-05-10 14:26:12 +0200

    Omogućena provjera unosa podataka

--- Frontend/prodev/src/Login/Login.jsx
+++ Frontend/prodev/src/Login/Login.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import loginSlika from "../logo.png";
+import { withRouter, Redirect } from 'react-router-dom';
+import {browserHistory} from 'react-router';
 
 export class Login extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    login = () =>  {
+        if (this.state.username === '' && this.state.password === '') alert('Molimo unesite vaše korisničke podatke!');
+        else if  (this.state.username === '') alert('Molimo unesite vaše korisničko ime!');
+        else if (this.state.password === '') alert('Molimo unesite vašu lozinku!');
+        else {
+            alert('Sve OK');
+        }
     }
 
     render() {
@@ -16,16 +37,22 @@ export class Login extends React.Component {
                 <div className="form">
                     <div className="form-group">
                         <label htmlFor="username">Korisničko ime</label>
-                        <input type="text" name="username"/>
+                        <input type="text" name="username" 
+                        value={this.state.username} 
+                        onChange={e => this.handleChange(e)}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Lozinka</label>
-                        <input type="password" name="password"/>
+                        <input type="password" name="password" 
+                        value={this.state.password} 
+                        onChange={e => this.handleChange(e)}
+                        />
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="button" className="btn">
+                <button type="button" className="btn" onClick={this.login}>
                     Login
                 </button>
             </div>
@@ -33,3 +60,4 @@ export class Login extends React.Component {
         );    
     }
 }
+
--- Frontend/prodev/src/Login/Register.jsx
+++ Frontend/prodev/src/Login/Register.jsx
@@ -4,6 +4,27 @@ import loginSlika from "../logo.png";
 export class Register extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+            email: ''
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    register = () => {
+        if (this.state.password === '' || this.state.email === '' || this.state.username === '') {
+            alert("Molimo unesite sve podatke!")
+        }
+        else {
+            alert("Sve OK!");
+        }
+
     }
 
     render() {
@@ -16,20 +37,30 @@ export class Register extends React.Component {
                 <div className="forma">
                     <div className="form-grupa">
                         <label htmlFor="username">Korisničko ime</label>
-                        <input type="text" name="username"/>
+                        <input type="text" 
+                        name="username"
+                        value={this.state.username} 
+                        onChange={e => this.handleChange(e)}
+                        />
                     </div>
                     <div className="form-grupa">
                         <label htmlFor="password">E-mail adresa</label>
-                        <input type="email" name="email"/>
+                        <input type="email" name="email"
+                        value={this.state.email} 
+                        onChange={e => this.handleChange(e)}
+                        />
                     </div>
                     <div className="form-grupa">
                         <label htmlFor="password">Lozinka</label>
-                        <input type="password" name="password"/>
+                        <input type="password" name="password"
+                        value={this.state.password} 
+                        onChange={e => this.handleChange(e)}
+                        />
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="button" className="btn">
+                <button type="button" className="btn" onClick={this.register}>
                     Register
                 </button>
             </div>
